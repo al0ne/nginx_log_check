2a9a27d2b95bb3acc3407fc5ae6935d4f025be68
al0ne <13449320+al0ne@users.noreply.github.com>
2019-12-09 13:05:20 +0800

    Update nginx_check.sh

--- nginx_check.sh
+++ nginx_check.sh
@@ -2,21 +2,22 @@
 
 echo ""
 echo " ========================================================= "
-echo " \                 Nginx日志安全分析脚本 V1.0                / "
+echo " \                 Nginx日志安全分析脚本 V1.0            / "
 echo " ========================================================= "
 echo " # 支持Nginx日志分析，攻击告警分析等                    "
 echo " # author：al0ne                    "
 echo " # https://github.com/al0ne                    "
-
+echo -e "\n"
 #此脚本是参考nmgxy/klionsec修改而来,重新添加了一些特征，只用来临时救急，还是推荐到ELK或者Splunk中分析
 
 #功能
 ###统计Top 20 地址
 ###SQL注入分析
-###扫描器告警分析
+###扫描器/常用黑客工具
 ###漏洞利用检测
 ###敏感路径访问
 ###文件包含攻击
+###HTTP Tunnel
 ###Webshell
 ###寻找响应长度的url Top 20
 ###寻找罕见的脚本文件访问
@@ -47,47 +48,57 @@ fi
 echo "分析结果日志：${outfile}"
 echo "Nginx日志目录：${access_dir}"
 echo "Nginx文件名：${access_log}"
+echo -e "\n"
 
 echo -e "\e[00;31m[+]TOP 20 IP 地址\e[00m"
 ag -a -o --nofilename '\d+\.\d+\.\d+\.\d+' ${access_dir}${access_log}* | sort | uniq -c | sort -nr | head -n 20 | tee -a ${outfile}/top20.log
 echo -e "\n"
 
 echo -e "\e[00;31m[+]SQL注入攻击分析\e[00m"
-ag -a "%20xor|%20and|%20AND|%20or|%20OR|select%20|SELECT%20|%20and%201=1|%20and%201=2|%20exec|%20from|%27exec|information_schema.tables|table_name|table_schema|%20where%20|%20union%20|%20UNION%20|concat\(|concat_ws\(|%20group%20|0x5f|0x7e|0x7c|0x27|%20limit|%20LIMIT|%20FROM|WHERE|version%28|version\(|database%28|database\(|user%28|user\(|%20extractvalue|%20updatexml|rand\(0\)\*2|%20group%20by%20x" ${access_dir}${access_log}* | awk '($9==200)||($9==500) {print $0}' | tee -a ${outfile}/sql.log
+ag -a "xp_cmdshell|%20xor|%20and|%20AND|%20or|%20OR|select%20|SELECT%20|%20and%201=1|%20and%201=2|%20exec|%20from|%27exec|information_schema.tables|load_file|benchmark|substring|table_name|table_schema|%20where%20|%20union%20|%20UNION%20|concat\(|concat_ws\(|%20group%20|0x5f|0x7e|0x7c|0x27|%20limit|%20LIMIT|%20FROM|version%28|version\(|database%28|database\(|user%28|user\(|%20extractvalue|%20updatexml|rand\(0\)\*2|%20group%20by%20x" ${access_dir}${access_log}* | awk '($9==200)||($9==500) {print $0}' >${outfile}/sql.log
 awk '{print "SQL注入攻击" NR"次"}' ${outfile}/sql.log | tail -n1
 echo "SQL注入 TOP 20 IP地址"
 ag -o '(?<=:)\d+\.\d+\.\d+\.\d+' ${outfile}/sql.log | sort | uniq -c | sort -nr | head -n 20 | tee -a ${outfile}/sql_top20.log
 echo -e "\n"
 
-echo -e "\e[00;31m[+]扫描器scan\e[00m"
-ag -a "sqlmap|acunetix|by_wvs|netsparker|HP404|nsfocus|WebCruiser|owasp|nmap|nessus|HEAD |AppScan|burpsuite|w3af|ZAP|openVAS|.+avij|.+angolin|360webscan|webscan|XSS@HERE|XSS%40HERE|wwwscan|wscan" ${access_dir}${access_log}* | awk '($9==200)||($9==500) {print $0}' >${outfile}/scan.log
+echo -e "\e[00;31m[+]扫描器scan & 黑客工具\e[00m"
+ag -a "sqlmap|acunetix|by_wvs|nikto|netsparker|HP404|nsfocus|WebCruiser|owasp|nmap|nessus|HEAD |AppScan|burpsuite|w3af|ZAP|openVAS|.+avij|.+angolin|360webscan|webscan|XSS@HERE|XSS%40HERE|NOSEC.JSky|wwwscan|wscan|antSword|WebVulnScan|WebInspect|ltx71|masscan|python-requests|Python-urllib|WinHttpRequest" ${access_dir}${access_log}* | awk '($9==200)||($9==500) {print $0}' >${outfile}/scan.log
 awk '{print "共检测到扫描攻击" NR"次"}' ${outfile}/scan.log | tail -n1
 echo "扫描工具流量 TOP 20"
 ag -o '(?<=:)\d+\.\d+\.\d+\.\d+' ${outfile}/scan.log | sort | uniq -c | sort -nr | head -n 20 | tee -a ${outfile}/scan_top20.log
 echo -e "\n"
 
 echo -e "\e[00;31m[+]敏感路径访问\e[00m"
-ag -a "/wls-wsat/|include=|phpinfo|info\.php|/web-console|/manager/html|axis2-admin|axis2-web|phpMyAdmin|phpmyadmin|/admin-console|/jmx-console|/console/|\.zip|\.rar|\.mdb|\.inc|\.sql|\.config|\.bak|/.svn/|/\.git/|\.hg|\.DS_Store|\.htaccess|nginx\.conf|\.bash_history|/CVS/|\.bak|wwwroot|备份|/Web.config|/web.config|/1.txt|/test.txt" ${access_dir}${access_log}* | awk '($9==200)||($9==500) {print $0}' | tee -a ${outfile}/dir.log
-awk '{print "共检测到针对敏感文件扫描" NR"次"}' ${outfile}dir.log | tail -n1
+ag -a "/_cat/|/_config/|include=|phpinfo|info\.php|/web-console|JMXInvokerServlet|/manager/html|axis2-admin|axis2-web|phpMyAdmin|phpmyadmin|/admin-console|/jmx-console|/console/|\.zip|\.rar|\.mdb|\.inc|\.sql|\.config|\.bak|/.svn/|/\.git/|\.hg|\.DS_Store|\.htaccess|nginx\.conf|\.bash_history|/CVS/|\.bak|wwwroot|备份|/Web.config|/web.config|/1.txt|/test.txt" ${access_dir}${access_log}* | awk '($9==200)||($9==500) {print $0}' >${outfile}/dir.log
+awk '{print "共检测到针对敏感文件扫描" NR"次"}' ${outfile}/dir.log | tail -n1
 echo "敏感文件访问流量 TOP 20"
 ag -o '(?<=:)\d+\.\d+\.\d+\.\d+' ${outfile}/dir.log | sort | uniq -c | sort -nr | head -n 20 | tee -a ${outfile}/dir_top20.log
 echo -e "\n"
 
 echo -e "\e[00;31m[+]漏洞利用检测\e[00m"
-ag -a "/wls-wsat/|uddiexplorer|@DEFAULT_MEMBER_ACCESS|@java\.lang\.Runtime|/bin/bash|cmd\.exe|wget\s|curl\s" ${access_dir}${access_log}* | awk '($9==200)||($9==500) {print $0}' | tee -a ${outfile}/exploit.log
+ag -a "%0D%0A|file:/|gopher:/|dict:/|WindowsPowerShell|/wls-wsat/|call_user_func_array\&vars\[0\]=|uddiexplorer|@DEFAULT_MEMBER_ACCESS|@java\.lang\.Runtime|OgnlContext|/bin/bash|cmd\.exe|wget\s|curl\s" ${access_dir}${access_log}* | awk '($9==200)||($9==500) {print $0}' >${outfile}/exploit.log
 awk '{print "漏洞利用探测" NR"次"}' ${outfile}/exploit.log | tail -n1
+echo "漏洞利用检测 TOP 20"
+ag -o '(?<=:)\d+\.\d+\.\d+\.\d+' ${outfile}/exploit.log | sort | uniq -c | sort -nr | head -n 20 | tee -a ${outfile}/exploit_top20.log
 echo -e "\n"
 
 echo -e "\e[00;31m[+]文件包含攻击\e[00m"
-ag -a "/passwd|%00|/win.ini|/my.ini|\.\./\.\./" ${access_dir}${access_log}* | awk '($9==200)||($9==500) {print $0}' | tee -a ${outfile}/lfi.log
+ag -a "/etc/passwd|/etc/shadow|%00|/win.ini|/my.ini|\.\./\.\./" ${access_dir}${access_log}* | awk '($9==200)||($9==500) {print $0}' >${outfile}/lfi.log
 awk '{print "共检测到LFI本地文件包含" NR"次"}' ${outfile}/lfi.log | tail -n1
 echo "文件包含 TOP 20"
 ag -o '(?<=:)\d+\.\d+\.\d+\.\d+' ${outfile}/lfi.log | sort | uniq -c | sort -nr | head -n 20 | tee -a ${outfile}/lfi_top20.log
 echo -e "\n"
 
 echo -e "\e[00;31m[+]webshell\e[00m"
-ag -a "exec=|cmd=|r57|c99|c100|b374k|eval(|assert\(|%eval|%execute|tunnel\.[asp|php|jsp|aspx]{3}|makewebtaski|ma\.[asp|php|jsp|aspx]{3}|cmd\.[asp|php|jsp|aspx]{3}|xiaoma\.[asp|php|jsp|aspx]{3}|tom\.[asp|php|jsp|aspx]{3}|k8cmd\.[asp|php|jsp|aspx]{3}|ver[0-9]{3}\.[asp|php|jsp|aspx]{3}|\.aar|^*.spy.*\..*p$" ${access_dir}${access_log}* | awk '($9==200)||($9==500) {print $0}' | tee -a ${outfile}/webshell.log
+ag -a "dbname=|exec=|cmd=|\br57\b|\bc99\b|\bc100\b|\bb374k\b|adminer.php|eval\(|assert\(|%eval|%execute|tunnel\.[asp|php|jsp|aspx]{3}|makewebtaski|ma\.[asp|php|jsp|aspx]{3}|\bup\.[asp|php|jsp|aspx]{3}|cmd\.[asp|php|jsp|aspx]{3}|xiaoma\.[asp|php|jsp|aspx]{3}|shell\.[asp|php|jsp|aspx]{3}|tom\.[asp|php|jsp|aspx]{3}|k8cmd\.[asp|php|jsp|aspx]{3}|ver[0-9]{3}\.[asp|php|jsp|aspx]{3}|\.aar|[asp|php|jsp|aspx]{3}spy\.|o=vLogin" ${access_dir}${access_log}* | awk '($9==200)||($9==500) {print $0}' >${outfile}/webshell.log
 awk '{print "共检测到webshell行为" NR "次"}' ${outfile}/webshell.log | tail -n1
+echo "Webshell TOP 20"
+ag -o '(?<=:)\d+\.\d+\.\d+\.\d+' ${outfile}/webshell.log | sort | uniq -c | sort -nr | head -n 20 | tee -a ${outfile}/webshell_top20.log
+echo -e "\n"
+
+echo -e "\e[00;31m[+]HTTP Tunnel\e[00m"
+ag -a "cmd=disconnect|cmd=read|cmd=forward|cmd=connect|127.0.0.1" ${access_dir}${access_log}* | awk '($9==200)||($9==500) {print $0}' | tee -a ${outfile}/tunnel.log
+awk '{print "共检测到隧道行为" NR "次"}' ${outfile}/tunnel.log | tail -n1
 echo -e "\n"
 
 echo -e "\e[00;31m[+]Top 20 url长度\e[00m"
@@ -97,10 +108,10 @@ echo -e "\n"
 
 echo -e "\e[00;31m[+]罕见的脚本文件访问\e[00m"
 echo "访问量特别特别少的脚本文件极有可能是webshell"
-cat ${access_dir}${access_log}* | awk '($9==200)||($9==500) {print $7}' | sort | uniq -c | sort -n | ag '\.php|\.jsp|\.asp|\.aspx' | head -n 20 | tee -a ${outfile}/rare_url.log
+cat ${access_dir}${access_log}* | awk '($9==200)||($9==500) {print $7}' | sort | uniq -c | sort -n | ag -v '\?' | ag '\.php|\.jsp|\.asp|\.aspx' | head -n 20 | tee -a ${outfile}/rare_url.log
 echo -e "\n"
 
 echo -e "\e[00;31m[+]302跳转\e[00m"
 echo "此目的是寻找一些登录成功的脚本文件，极有可能是webshell文件"
-cat ${access_dir}${access_log}* | awk '($9==302)||($9==301) {print $7}' | sort | uniq -c | sort -n | ag '\.php|\.jsp|\.asp|\.aspx' | head -n 20 | tee -a ${outfile}/302_goto.log
+cat ${access_dir}${access_log}* | awk '($9==302)||($9==301) {print $7}' | sort | uniq -c | sort -n | ag -v '\?' | ag '\.php|\.jsp|\.asp|\.aspx' | head -n 20 | tee -a ${outfile}/302_goto.log
 echo -e "\n"
